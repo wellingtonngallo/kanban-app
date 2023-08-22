34f4e7f7d80c79f49f54344a2752e4bb58a31bb1
Wellington Gallo <wellington.gallo@bancointer.com.br>
2023-08-22 15:54:10 -0300

    fix: tests

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -5,20 +5,32 @@ import * as useAuthModule from "../../hooks/useAuth";
 import * as useSearchModule from "../../hooks/useSearch";
 import { Header } from ".";
 import { type AuthContextData } from "../../context/AuthContext";
+import { MemoryRouter } from "react-router-dom";
 
 describe("tests Header", () => {
   beforeEach(() => {
     const useAuthMock = jest.spyOn(useAuthModule, "useAuth");
+    const mockNavigate = jest.fn();
+
     useAuthMock.mockReturnValue({
       user: {
         displayName: "Gallo",
         photoUrl: "https://via.placeholder.com/40",
       },
     } as AuthContextData);
+
+    jest.mock("react-router-dom", () => ({
+      ...jest.requireActual("react-router-dom"),
+      useNavigate: () => mockNavigate,
+    }));
   });
 
   it("should renders title", () => {
-    render(<Header />);
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>,
+    );
 
     const titleElement = screen.getByRole("heading", { name: /Kanban APP/i });
 
@@ -26,7 +38,11 @@ describe("tests Header", () => {
   });
 
   it("should render displayName", () => {
-    render(<Header />);
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>,
+    );
 
     const displayNameElement = screen.getByText("Gallo");
 
@@ -34,7 +50,11 @@ describe("tests Header", () => {
   });
 
   it("should render photo", () => {
-    render(<Header />);
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>,
+    );
 
     const photoElement = screen.getByRole("img", { name: /Foto do usuário/i });
 
@@ -53,7 +73,11 @@ describe("tests Header", () => {
       searchTask: "",
     });
 
-    render(<Header />);
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>,
+    );
 
     const searchInput = screen.getByPlaceholderText("Buscar tarefa");
     fireEvent.change(searchInput, { target: { value: "Teste kanban-app" } });
